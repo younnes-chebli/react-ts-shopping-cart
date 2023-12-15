1c0425ed1e5133293f29eb32ac212ca9d96e4001
Younnes Chebli <younnesc@gmail.com>
2023-12-15 13:09:46 +0100

    feat(fetch products): fetching products from json-server

--- README.md
+++ README.md
@@ -1,7 +1,13 @@
 # Shopping Cart
 
 ## Technologies
+
 - React.js
 - TypeScript
 
 ## The App
+
+## Misc
+
+- To run json-server without installing the dependency:
+  `npx json-server -p 3500 -w data/products.json`
--- src/context/ProductsProvider.tsx
+++ src/context/ProductsProvider.tsx
@@ -1,4 +1,4 @@
-import { ReactElement, createContext, useState } from "react"
+import { ReactElement, createContext, useEffect, useState } from "react"
 
 export type ProductType = {
     sku: string,
@@ -6,7 +6,8 @@ export type ProductType = {
     price: number
 }
 
-const initState: ProductType[] = [
+const initState: ProductType[] = []
+/* const initState: ProductType[] = [
     {
         "sku": "item0001",
         "name": "Widget",
@@ -22,7 +23,7 @@ const initState: ProductType[] = [
         "name": "Deluxe Widget",
         "price": 29.99
     }
-]
+] */
 
 export type UseProductsContextType = { products: ProductType[] }
 
@@ -34,6 +35,22 @@ type ChildrenType = { children?: ReactElement | ReactElement[] }
 
 export const ProductsProvider = ({ children }: ChildrenType): ReactElement => {
     const [products, setProducts] = useState<ProductType[]>(initState)
+
+    useEffect(() => {
+        const fetchProducts = async (): Promise<ProductType[]> => {
+            const data = await fetch('http://localhost:3500/products')
+                .then(res => res.json())
+                .catch(err => {
+                    if (err instanceof Error) console.log(err.message);
+
+                })
+
+            return data
+        }
+
+        fetchProducts()
+            .then(products => setProducts(products))
+    }, [])
 
     return (
         <ProductsContext.Provider value={{ products }} >
