7c83add574f1a8de9d2caf8d295fbe27dfd8bfce
Younnes Chebli <younnesc@gmail.com>
2023-12-19 09:19:17 +0100

    feat: added Footer component setup

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,31 @@
-const Footer = () => {
-    return (
-        <div>Footer</div>
+import useCart from "../hooks/useCart"
+
+type PropsType = {
+    viewCart: boolean
+}
+
+const Footer = ({ viewCart }: PropsType) => {
+    const { totalItems, totalPrice } = useCart()
+
+    const year: number = new Date().getFullYear()
+
+    const footerContent = viewCart
+        ? <p>YNS Shopping Cart &copy; {year}</p>
+        : (
+            <>
+                <p>Total Items: {totalItems}</p>
+                <p>Total Price: {totalPrice}</p>
+                <p>YNS Shopping Cart &copy; {year}</p>
+            </>
+        )
+
+    const content = (
+        <footer className="footer">
+            {footerContent}
+        </footer>
     )
+
+    return content
 }
 
 export default Footer
