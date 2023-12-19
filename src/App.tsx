bcf955e65d050d952b37e19667320f759a2e1b81
Younnes Chebli <younnesc@gmail.com>
2023-12-19 06:45:02 +0100

    feat: app component setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,23 @@
+import { useState } from "react"
+import Cart from "./components/Cart"
+import ProductList from "./components/ProductList"
+import Footer from "./components/Footer"
+import Header from "./components/Header"
+
 const App = () => {
-  return (
-    <div className="App">App</div>
+  const [viewCart, setViewCart] = useState(false)
+
+  const pageContent = viewCart ? <Cart /> : <ProductList />
+
+  const content = (
+    <>
+      <Header viewCart={viewCart} setViewCart={setViewCart} />
+      {pageContent}
+      <Footer viewCart={viewCart} />
+    </>
   )
+
+  return content
 }
 
 export default App
