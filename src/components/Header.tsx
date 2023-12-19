b88116826aba1f1237c67f8ed34a2457302369ca
Younnes Chebli <younnesc@gmail.com>
2023-12-19 07:00:16 +0100

    feat: Header and Nav components setup

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,25 @@
-const Header = () => {
-    return (
-        <div>Header</div>
+import Nav from "./Nav"
+
+type PropsType = {
+    viewCart: boolean,
+    setViewCart: React.Dispatch<React.SetStateAction<boolean>>
+}
+
+const Header = ({ viewCart, setViewCart }: PropsType) => {
+    const content = (
+        <header className="header">
+            <div className="header__title-bar">
+                <h1>YNS Co.</h1>
+                <div className="header__price-box">
+                    <p>Total Items:</p>
+                    <p>Total Price:</p>
+                </div>
+            </div>
+            <Nav viewCart={viewCart} setViewCart={setViewCart} />
+        </header>
     )
+
+    return content
 }
 
 export default Header
